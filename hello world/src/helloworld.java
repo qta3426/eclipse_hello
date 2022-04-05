ca2ff8e57d9a485ff4a3c1469ede5f57db6f39eb
asd <asd@naver.com>
2022-04-05 10:00:14 +0900

    conflict resolved

--- hello world/src/helloworld.java
+++ hello world/src/helloworld.java
@@ -3,10 +3,7 @@ public class helloworld {
 	public static void main(String[] args) {
 	System.out.println("Hello world");
 	System.out.println("Hello his world");
-<<<<<<< HEAD
 	System.out.println("Hello your world");
-=======
 	System.out.println("Hello her world");
->>>>>>> refs/heads/hotfix
 	}
 }
