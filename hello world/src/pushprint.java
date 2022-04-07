2dd855f1dfd014efd567d1929d6551776291733f
asd <asd@naver.com>
2022-04-07 10:57:14 +0900

    Added pushprint.java

---
+++ hello world/src/pushprint.java
@@ -0,0 +1,9 @@
+
+public class helloworld {
+	public static void main(String[] args) {
+	System.out.println("Hello world");
+	System.out.println("Hello his world");
+	System.out.println("Hello your world");
+	System.out.println("Hello her world");
+	}
+}
